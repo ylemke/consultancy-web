bc24cceb7605ddebe4c68787eb733699d39e9706
ylemke <yasmin.lemkec@gmail.com>
2026-03-18 11:28:54 +0100

    Add CoursesPage route to App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,6 +5,7 @@ import { Navigation } from "./components/Navigation";
 import { Footer } from "./components/Footer";
 import HomePage from "./pages/HomePage";
 import MentoringPage from "./pages/MentoringPage";
+import CoursesPage from "./pages/CoursesPage";
 import AboutPage from "./pages/AboutPage";
 import ContactPage from "./pages/ContactPage";
 import NotFoundPage from "./pages/NotFoundPage";
@@ -19,6 +20,7 @@ function App() {
             <Routes>
               <Route path="/" element={<HomePage />} />
               <Route path="/mentoring" element={<MentoringPage />} />
+              <Route path="/courses" element={<CoursesPage />} />
               <Route path="/about" element={<AboutPage />} />
               <Route path="/contact" element={<ContactPage />} />
               <Route path="*" element={<NotFoundPage />} />
